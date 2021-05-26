54ca8bc8d30f80e0cfea584d082f06c4c96ffca7
csgtdev04 <saitanujm@gmail.com>
2021-05-26 17:57:50 -0500

    first, second, third class

---
+++ HelloWorld.py
@@ -0,0 +1,50 @@
+import math
+# boolean values = True, False
+
+# numbers = -infinity to +infinity
+
+# words = strings 
+
+print("Sai is my name")
+
+# calculations
+print(2 + 3)
+print(4 - 3)
+print(3 * 8) # * - SHIFT 8
+print(6 / 3)
+
+print(round(0.6666, 2))
+
+print(17 % 2) # shift 5
+
+print(13 // 2)
+print(2 ** 2)
+
+name="Sai"
+age=17
+
+print(name)
+print(age)
+
+# doesn't
+
+print(math.sqrt(16))
+print(math.pi)
+print(math.ceil(3.2))
+print(math.floor(3.9999999))
+
+
+# Warm up exercises (tell me what gets printed out!)
+print(6 / 3) 
+
+print(13 // 2)
+
+print(4 ** 2)
+
+print(3 * 'un' + 'ium') # unununium
+
+print(round(0.6666, 3)) # 0.667
+
+print(math.floor(3.9999999))
+
+print(math.ceil(3.01))
---
+++ SecondClass.py
@@ -0,0 +1,49 @@
+some_word = "balloon"
+# some_word = "hi" # immutable
+
+my_number = 76
+
+word = "Python"
+
+
+print(word[1])
+print(word[2])
+            
+new_word = "Superman and Superwoman"
+# print out 'a' in Superman
+print(new_word[6])
+# print out 'w' in Superwoman
+print(new_word[18])
+#-------------------------------          
+another_word = "Turkey in Thanksgiving"
+# print out the 'n' in in
+print(another_word[8])
+# print out the 'v' in thanksgiving
+print(another_word[18])
+
+
+name = "MSai"
+
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+
+game = "Minecraft"
+# print out 'f'
+print(game[4:9])
+print(game[0:4])
+
+print(game[0:4] + game[4:9])
+
+cool_word = "Cookies and Cream"
+# print 'Cream'
+print(cool_word[12:])
+# print 'and'
+print(cool_word[8:11])
+# print 'Cookies'
+print(cool_word[:7])
+
+
+
+
