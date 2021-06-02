d27dfcbe56b293f775a36b2cf37e1df076b58a4f
csgtdev04 <saitanujm@gmail.com>
2021-06-02 12:54:33 -0500

    adding third class

---
+++ ThirdClass.py
@@ -0,0 +1,43 @@
+description = "Super Cool & Awesome ipfhpfhjpfj[J[j[u"
+               
+# print out the '&'
+print(description[11])
+# print out the word 'Cool'
+print (description[6:9])
+# print out the word 'wes' in Awesome
+print(description[14:17]) 
+
+print(description[:5])
+
+print(description[13:])
+
+name = "Sai"
+print(name[-3])
+
+print(len(description))
+print(len(name))
+
+# --------------------------
+
+# lists
+item1 = "milk"
+item2 = "banana"
+item3 = "apple"
+
+grocery_list = [item1, item2, item3, "m&m", "bread"]
+print("grocery list: ", grocery_list)
+
+numbers = [1, 2, 3, 56, 76, 23]
+print(numbers)
+
+list_inside_list = [2, 3, 4, ["Sai", 43, False]]
+print(list_inside_list)
+
+art_supplies = ['brushes', 'pens', 'markers', 'paper']
+print(art_supplies)
+
+summer_plans = ["relax", "have fun", "go outside", 50, True]
+print(summer_plans)
+
+cheeses=["swiss","cheddar","string","blue","mozzerella"]
+print(cheeses)
